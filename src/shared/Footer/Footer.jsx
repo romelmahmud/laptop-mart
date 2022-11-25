15986b15e3ed4d72a85cc52cfa7d9bf86988f1ab
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-25 14:43:29 +0600

    footer done

--- src/shared/Footer/Footer.jsx
+++ src/shared/Footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import logo from "../../assets/images/logo.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer footer-center p-10 bg-violet-100  text-violet-900">
+      <div>
+        <img src={logo} alt="" className="w-20" />
+        <p className="font-bold">Laptop Mart </p>
+        <p>Copyright © 2022 - All right reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
