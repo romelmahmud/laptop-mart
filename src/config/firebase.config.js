c7baad0e520909b914b8a56f0cf7a20f90036bcf
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-26 14:27:30 +0600

    dot env error fixed

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -5,19 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVsUBHm1_Zxrf3heLQCArdPmTisWtRnfU",
-  authDomain: "laptop-mart-388bb.firebaseapp.com",
-  projectId: "laptop-mart-388bb",
-  storageBucket: "laptop-mart-388bb.appspot.com",
-  messagingSenderId: "105365781",
-  appId: "1:105365781:web:296bfa80aa8e8f95b5691c",
-
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
