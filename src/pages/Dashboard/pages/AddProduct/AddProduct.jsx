050f2dd4e1eb349cc27a49f300dca6ee960311a2
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-27 00:30:12 +0600

    Add a product form design complete

--- src/pages/Dashboard/pages/AddProduct/AddProduct.jsx
+++ src/pages/Dashboard/pages/AddProduct/AddProduct.jsx
@@ -22,12 +22,28 @@ const AddProduct = () => {
             // onSubmit={handleSubmit(handleRegister)}
             className="mt-4 grid grid-cols-12 gap-6"
           >
-            <div className="col-span-12">
+            <div className="col-span-12 md:col-span-6 lg:col-span-4">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Upload product image
+                </span>
+              </label>
+              <input
+                type="file"
+                className="file-input file-input-bordered w-full border-gray-400 bg-gray-50"
+              />
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-8"></div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-6">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Product Title
+                </span>
+              </label>
               <input
                 type="text"
-                placeholder="name"
                 {...register("name", {
-                  required: "Name is Required",
+                  required: "Product title is Required",
                 })}
                 className="input input-bordered w-full border-gray-400 bg-gray-50 "
               />
@@ -35,38 +51,13 @@ const AddProduct = () => {
                 <p className="text-red-500">{errors.name.message}</p>
               )}
             </div>
-            <div className="col-span-12">
-              <input
-                type="text"
-                placeholder="email"
-                {...register("email", {
-                  required: true,
-                })}
-                className="input input-bordered w-full border-gray-400 bg-gray-50 "
-              />
-              {errors.email && (
-                <p className="text-red-500">{errors.email.message}</p>
-              )}
-            </div>
-
-            <div className="col-span-12 ">
-              <input
-                type="password"
-                placeholder="password"
-                {...register("password", {
-                  required: "Password is required",
-                  minLength: {
-                    value: 6,
-                    message: "Password must be 6 characters long",
-                  },
-                })}
-                className="input input-bordered w-full border-gray-400 bg-gray-50 "
-              />
-              {errors.password && (
-                <p className="text-red-500">{errors.password.message}</p>
-              )}
-            </div>
-            <div className="col-span-9 ">
+            <div className="col-span-12 md:col-span-6"></div>
+            <div className=" col-span-12 md:col-span-6 lg:col-span-3">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Category
+                </span>
+              </label>
               <select
                 {...register("role")}
                 className="select select-bordered w-full border-gray-400 bg-gray-50"
@@ -77,43 +68,138 @@ const AddProduct = () => {
                 <option value="seller">Seller</option>
               </select>
             </div>
+            <div className=" col-span-12 md:col-span-6 lg:col-span-3 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Condition
+                </span>
+              </label>
+              <select
+                {...register("condition")}
+                className="select select-bordered w-full border-gray-400 bg-gray-50"
+              >
+                <option></option>
+                <option value="excellent">Excellent</option>
+                <option value="good">Good</option>
+                <option value="fair">Fair</option>
+              </select>
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-6"></div>
 
-            <div className="col-span-12 sm:flex sm:items-center sm:gap-4">
+            <div className="col-span-12 md:col-span-6 lg:col-span-4 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Original Price
+                </span>
+              </label>
+              <input
+                type="number"
+                {...register("originalPrice", {
+                  required: true,
+                })}
+                className="input input-bordered w-full border-gray-400 bg-gray-50 "
+              />
+              {errors.originalPrice && (
+                <p className="text-red-500">{errors.originalPrice.message}</p>
+              )}
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Resale Price
+                </span>
+              </label>
+              <input
+                type=""
+                {...register("resalePrice", {
+                  required: true,
+                })}
+                className="input input-bordered w-full border-gray-400 bg-gray-50 "
+              />
+              {errors.resalePrice && (
+                <p className="text-red-500">{errors.resalePrice.message}</p>
+              )}
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4"></div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Purchase Year
+                </span>
+              </label>
+              <input
+                type=""
+                {...register("purchaseYear", {
+                  required: true,
+                })}
+                className="input input-bordered w-full border-gray-400 bg-gray-50 "
+              />
+              {errors.purchaseYear && (
+                <p className="text-red-500">{errors.purchaseYear.message}</p>
+              )}
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Years of use
+                </span>
+              </label>
+              <input
+                type=""
+                {...register("yearOfUses", {
+                  required: true,
+                })}
+                className="input input-bordered w-full border-gray-400 bg-gray-50 "
+              />
+              {errors.yearOfUses && (
+                <p className="text-red-500">{errors.yearOfUses.message}</p>
+              )}
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4"></div>
+
+            <div className="col-span-12 md:col-span-6 lg:col-span-4 ">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Location
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder="example(Dhaka,Bangladesh)"
+                {...register("location", {
+                  required: "location is required",
+                })}
+                className="input input-bordered w-full border-gray-400 bg-gray-50 "
+              />
+              {errors.location && (
+                <p className="text-red-500">{errors.location.message}</p>
+              )}
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-8"></div>
+
+            <div className="col-span-12 md:col-span-8">
+              <label className="label">
+                <span className="label-text text-lg font-medium text-gray-500">
+                  Description
+                </span>
+              </label>
+              <textarea
+                rows="4"
+                cols="100"
+                className="textarea textarea-bordered bg-gray-50"
+              ></textarea>
+            </div>
+            <div className="col-span-12 md:col-span-6 lg:col-span-4"></div>
+
+            <div className="col-span-12 md:col-span-6 lg:col-span-4">
               <button
                 type="submit"
                 className="inline-block shrink-0 rounded-md bottom-1 border border-gray-400 px-8 py-3 text-lg font-medium text-gray-500 transition hover:bg-violet-900 hover:text-white focus:outline-none focus:ring active:text-violet-900 w-full "
               >
-                Register
+                Add Product
               </button>
             </div>
-            <div className="col-span-12 ">
-              <p className="mt-4 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-                Already have an account?
-                <Link
-                  to={"/login"}
-                  className=" text-violet-900 font-semibold  underline "
-                >
-                  <span className="ml-1 text-md">Login</span>
-                </Link>
-              </p>
-            </div>
           </form>
-          <div className="divider text-gray-600">OR</div>
-
-          <div className="flex justify-between ">
-            {/* <button
-          // onClick={handleGoogleLogin}
-          className="py-3 px-6  border border-gray-400  mr-4 text-gray-500 text-lg font-medium hover:bg-gray-200 transition rounded hover:text-violet-900 w-full"
-        >
-          {" "}
-          <img
-            src={googleLogo}
-            alt=""
-            className="h-6 w-6 inline-block mr-3"
-          />
-          Login with Google
-        </button> */}
-          </div>
         </div>
       </div>
     </div>
