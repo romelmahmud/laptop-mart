ab9feb0920540e89009e55fbb33cb20460267b11
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-28 05:31:44 +0600

    getting product for category done

--- src/pages/Categories/Categories.jsx
+++ src/pages/Categories/Categories.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Spinner from "../../shared/Spinner/Spinner";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const { id } = useParams();
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products", id],
+    queryFn: async () => {
+      const data = await axios.get(
+        `http://localhost:8000/category/products/${id}`,
+        {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      return data.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return <div>{products.length}</div>;
 };
 
 export default Categories;
--- src/pages/Home/components/Categories/CategoryButton.jsx
+++ src/pages/Home/components/Categories/CategoryButton.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CategoryButton = ({ category }) => {
   return (
-    <div className=" bg-violet-100-50 flex justify-center items-center py-6 md:py-8 lg:py-12 text-2xl font-medium text-gray-700 rounded-md border-2 border-violet-900 shadow-md cursor-pointer hover:bg-violet-900 transition hover:text-white">
-      {category.name}
-    </div>
+    <Link to={`/categories/${category._id}`}>
+      <div className=" bg-violet-100-50 flex justify-center items-center py-6 md:py-8 lg:py-12 text-2xl font-medium text-gray-700 rounded-md border-2 border-violet-900 shadow-md cursor-pointer hover:bg-violet-900 transition hover:text-white">
+        {category.name}
+      </div>
+    </Link>
   );
 };
 
