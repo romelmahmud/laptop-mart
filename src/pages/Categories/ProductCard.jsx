89b1ffac592db6414d6ef1be03506d933463abcf
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-28 14:32:16 +0600

    product advertise when button clicked done

--- src/pages/Categories/ProductCard.jsx
+++ src/pages/Categories/ProductCard.jsx
@@ -66,7 +66,26 @@ const ProductCard = ({ product }) => {
         </p>
 
         <div className="card-actions justify-end">
-          <button className="btn bg-violet-900">Book Now</button>
+          <label htmlFor="bookModal" className="btn bg-violet-900">
+            Book Now
+          </label>
+        </div>
+      </div>
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Congratulations random Internet user!
+          </h3>
+          <p className="py-4">
+            You've been selected for a chance to get one year of subscription to
+            use Wikipedia for free!
+          </p>
+          <div className="modal-action">
+            <label htmlFor="my-modal-6" className="btn">
+              Yay!
+            </label>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Dashboard/pages/MyProducts/MyProducts.jsx
+++ src/pages/Dashboard/pages/MyProducts/MyProducts.jsx
@@ -1,7 +1,104 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { AuthContext } from "../../../../context/auth/authContext";
+import Spinner from "../../../../shared/Spinner/Spinner";
+import toast from "react-hot-toast";
 
 const MyProducts = () => {
-  return <div>MyProducts</div>;
+  const { user } = useContext(AuthContext);
+
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["myproducts"],
+    queryFn: async () => {
+      const data = await axios.get(
+        `http://localhost:8000/seller/products/${user.email}`,
+        {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      return data.data;
+    },
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const handleAdvertiseUpdate = (id) => {
+    axios
+      .get(`http://localhost:8000/products/advertise/${id}`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.acknowledged) {
+          toast.success("Product advertise successfully");
+          refetch();
+        }
+      });
+  };
+
+  return (
+    <div className="py-20">
+      <h2 className="text-center text-2xl font-semibold">
+        You have {products.length} products
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product, i) => (
+              <tr key={product._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className=" w-12 h-12">
+                      <img
+                        src={product.productImage}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{product.name}</td>
+                <td>{product.resalePrice}</td>
+                <td>{product.status}</td>
+                <td>
+                  {product.status === "unsold" ? (
+                    <button
+                      onClick={() => handleAdvertiseUpdate(product._id)}
+                      className="btn btn-outline btn-sm "
+                    >
+                      Advertise
+                    </button>
+                  ) : (
+                    <button></button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyProducts;
