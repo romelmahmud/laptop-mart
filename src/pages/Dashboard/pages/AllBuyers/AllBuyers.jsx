60f6344965c5846b8de0822189b418c2e27da178
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-28 18:31:34 +0600

    All buyers page desgin complete

--- src/pages/Dashboard/pages/AllBuyers/AllBuyers.jsx
+++ src/pages/Dashboard/pages/AllBuyers/AllBuyers.jsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { AuthContext } from "../../../../context/auth/authContext";
+import Spinner from "../../../../shared/Spinner/Spinner";
+import Avatar from "../../../../assets/images/avatar.jpg";
+import toast from "react-hot-toast";
 
 const AllBuyers = () => {
-  return <div>AllBuyers</div>;
+  const { user } = useContext(AuthContext);
+
+  const {
+    data: buyers,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allBuyers"],
+    queryFn: async () => {
+      const data = await axios.get(`http://localhost:8000/users/buyers`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      return data.data;
+    },
+  });
+
+  const handleDelete = (id) => {
+    // axios
+    //   .delete(`http://localhost:8000/seller/products/${id}`, {
+    //     headers: {
+    //       authorization: `bearer ${localStorage.getItem("accessToken")}`,
+    //     },
+    //   })
+    //   .then((res) => {
+    //     if (res.data.acknowledged) {
+    //       toast.success("Product Deleted successfully");
+    //       refetch();
+    //     }
+    //   });
+  };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="py-20">
+      <h2 className="text-center text-2xl font-semibold mb-5">
+        Total {buyers.length} Sellers
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Location</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {buyers?.map((buyer, i) => (
+              <tr key={buyer._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className=" w-12 h-12 rounded-full">
+                      <img
+                        src={buyer.photo_url ? buyer.photo_url : Avatar}
+                        alt={buyer.name}
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{buyer.name}</td>
+                <td>{buyer.email}</td>
+                <td>{buyer.location}</td>
+
+                <td>
+                  <button
+                    onClick={() => handleDelete(buyer._id)}
+                    className="btn btn-error btn-outline btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllBuyers;
--- src/pages/Dashboard/pages/AllSellers/AllSellers.jsx
+++ src/pages/Dashboard/pages/AllSellers/AllSellers.jsx
@@ -1,7 +1,127 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { AuthContext } from "../../../../context/auth/authContext";
+import Spinner from "../../../../shared/Spinner/Spinner";
+import toast from "react-hot-toast";
 
-const AllSellers = () => {
-  return <div>AllSellers</div>;
+const AllSeller = () => {
+  const { user } = useContext(AuthContext);
+
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["allSeller"],
+    queryFn: async () => {
+      const data = await axios.get(
+        `http://localhost:8000/seller/products/${user.email}`,
+        {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("accessToken")}`,
+          },
+        }
+      );
+      return data.data;
+    },
+  });
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:8000/seller/products/${id}`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.acknowledged) {
+          toast.success("Product Deleted successfully");
+          refetch();
+        }
+      });
+  };
+
+  const handleAdvertiseUpdate = (id) => {
+    axios
+      .get(`http://localhost:8000/products/advertise/${id}`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      })
+      .then((res) => {
+        if (res.data.acknowledged) {
+          toast.success("Product advertise successfully");
+          refetch();
+        }
+      });
+  };
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="py-20">
+      <h2 className="text-center text-2xl font-semibold">
+        You have {products.length} products
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Status</th>
+              <th>Advertise</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products?.map((product, i) => (
+              <tr key={product._id}>
+                <th>{i + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className=" w-12 h-12">
+                      <img
+                        src={product.productImage}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{product.name}</td>
+                <td>{product.resalePrice}</td>
+                <td>{product.status}</td>
+                <td>
+                  {product.status === "unsold" ? (
+                    <button
+                      onClick={() => handleAdvertiseUpdate(product._id)}
+                      className="btn btn-outline btn-sm btn-primary "
+                    >
+                      Advertise
+                    </button>
+                  ) : (
+                    <button></button>
+                  )}
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(product._id)}
+                    className="btn btn-error btn-outline btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
-export default AllSellers;
+export default AllSeller;
